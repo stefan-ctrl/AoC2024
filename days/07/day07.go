98c06b5f764620c13ebb88aa5442f32ef5d41acc
Stefan Siber <stefan-siber@t-online.de>
2025-01-12 11:36:45 +0200

    feat: day07

---
+++ days/07/day07.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"AoC2024/util"
+	"fmt"
+	"log"
+	"math"
+	"strconv"
+	"strings"
+)
+
+const PLUS = "+"
+const MULTIPLY = "*"
+const CONCATENATION = "|"
+
+var legalOperatorsTask01 = []string{PLUS, MULTIPLY}
+var legalOperatorsTask02 = []string{PLUS, MULTIPLY, CONCATENATION}
+
+type CalibratorEquations struct {
+	checkValue int
+	operators  []int
+}
+
+func intToNewBaseFixedLength(number, base, length int) string {
+	if length == 0 {
+		return "0"
+	}
+
+	otherBasis := strconv.FormatInt(int64(number), base)
+	for len(otherBasis) < length {
+		otherBasis = "0" + otherBasis
+	}
+	return otherBasis
+}
+
+func newCalibratorEquations(str string) *CalibratorEquations {
+	str = strings.Replace(str, ":", "", -1)
+	split := strings.Split(str, " ")
+
+	cv, _ := strconv.Atoi(split[0])
+	operators := make([]int, 0)
+	for i := 1; i < len(split); i++ {
+		o, _ := strconv.Atoi(split[i])
+		operators = append(operators, o)
+	}
+
+	return &CalibratorEquations{
+		checkValue: cv,
+		operators:  operators,
+	}
+}
+
+func (c *CalibratorEquations) IsCheckValueReachable(mathOperatorList []string) bool {
+	base := len(mathOperatorList)
+	pow := len(c.operators) - 1
+	validVariations := int(math.Pow(float64(base), float64(pow)))
+	for i := 0; i < validVariations; i++ {
+		mutationSeed := intToNewBaseFixedLength(i, base, pow)
+		mutatedMathOperators := toMathOperators(mutationSeed)
+		if c.checkValue == c.solve(mutatedMathOperators) {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *CalibratorEquations) solve(mathOperators string) int {
+	result := c.operators[0]
+	for i := range mathOperators {
+		switch string(mathOperators[i]) {
+		case PLUS:
+			result += c.operators[i+1]
+		case MULTIPLY:
+			result *= c.operators[i+1]
+		case CONCATENATION:
+			leftSide := strconv.Itoa(result)
+			rightSide := strconv.Itoa(c.operators[i+1])
+			tmp, _ := strconv.Atoi(leftSide + rightSide)
+			result = tmp
+		default:
+			panic("not implemented")
+		}
+	}
+	return result
+}
+
+func toMathOperators(binaryMutationSeed string) string {
+	mutatedOperators := ""
+	for i := range binaryMutationSeed {
+		switch binaryMutationSeed[i] {
+		case '0':
+			mutatedOperators += PLUS
+		case '1':
+			mutatedOperators += MULTIPLY
+		case '2':
+			mutatedOperators += CONCATENATION
+		default:
+			panic("not implemented")
+		}
+	}
+	return mutatedOperators
+}
+
+func main() {
+	lines, err := util.ReadFilePerLine("./input/day07.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ces := make([]*CalibratorEquations, 0)
+	for i := 0; i < len(lines); i++ {
+		ces = append(ces, newCalibratorEquations(lines[i]))
+	}
+
+	result := task01(ces)
+	fmt.Println(result)
+	result = task02(ces)
+	fmt.Println(result)
+}
+
+func task01(ces []*CalibratorEquations) int {
+	result := 0
+	for i := range ces {
+		if ces[i].IsCheckValueReachable(legalOperatorsTask01) {
+			result += ces[i].checkValue
+		}
+	}
+	return result
+}
+
+func task02(ces []*CalibratorEquations) int {
+	result := 0
+	for i := range ces {
+		if ces[i].IsCheckValueReachable(legalOperatorsTask02) {
+			result += ces[i].checkValue
+		}
+	}
+	return result
+}
---
+++ days/07/day07_test.go
@@ -0,0 +1,299 @@
+package main
+
+import (
+	"AoC2024/util"
+	"log"
+	"reflect"
+	"testing"
+)
+
+func Test_newCalibratorEquations(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *CalibratorEquations
+	}{
+		{
+			name: "empty",
+			want: &CalibratorEquations{
+				checkValue: 0,
+				operators:  make([]int, 0),
+			},
+		},
+		{
+			name: "AOC First Line",
+			args: args{
+				str: "190: 10 19",
+			},
+			want: &CalibratorEquations{
+				checkValue: 190,
+				operators:  []int{10, 19},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newCalibratorEquations(tt.args.str); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newCalibratorEquations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_intToBinaryFixedLength(t *testing.T) {
+	type args struct {
+		number int
+		base   int
+		length int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "0 -> 0",
+			args: args{
+				number: 0,
+				base:   2,
+				length: 0,
+			},
+			want: "0",
+		},
+		{
+			name: "2 -> 10",
+			args: args{
+				number: 2,
+				base:   2,
+				length: 1,
+			},
+			want: "10",
+		},
+		{
+			name: "10 -> 0001010",
+			args: args{
+				number: 10,
+				base:   2,
+				length: 7,
+			},
+			want: "0001010",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := intToNewBaseFixedLength(tt.args.number, tt.args.base, tt.args.length); got != tt.want {
+				t.Errorf("intToNewBaseFixedLength() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_toMathOperators(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "00",
+			want: PLUS + PLUS,
+		},
+		{
+			name: "010110",
+			want: PLUS + MULTIPLY + PLUS + MULTIPLY + MULTIPLY + PLUS,
+		},
+		{
+			name: "020112",
+			want: PLUS + CONCATENATION + PLUS + MULTIPLY + MULTIPLY + CONCATENATION,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toMathOperators(tt.name); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("toMathOperators() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCalibratorEquations_solve(t *testing.T) {
+	type fields struct {
+		checkValue int
+		operators  []int
+	}
+	type args struct {
+		mathOperators string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   int
+	}{
+		{
+			name: "AOC Task 1 First Line",
+			fields: fields{
+				checkValue: 190,
+				operators:  []int{10, 19},
+			},
+			args: args{
+				mathOperators: MULTIPLY,
+			},
+			want: 190,
+		},
+		{
+			name: "AOC Task 1 First Line with plus",
+			fields: fields{
+				checkValue: 190,
+				operators:  []int{10, 19},
+			},
+			args: args{
+				mathOperators: PLUS,
+			},
+			want: 29,
+		},
+		{
+			name: "AOC Task 2 First Line with CONCATENATION",
+			fields: fields{
+				checkValue: 190,
+				operators:  []int{10, 19},
+			},
+			args: args{
+				mathOperators: CONCATENATION,
+			},
+			want: 1019,
+		},
+		{
+			name: "AOC Task 1  Example: 292",
+			fields: fields{
+				checkValue: 292,
+				operators:  []int{11, 6, 16, 20},
+			},
+			args: args{
+				mathOperators: PLUS + MULTIPLY + PLUS,
+			},
+			want: 292,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &CalibratorEquations{
+				checkValue: tt.fields.checkValue,
+				operators:  tt.fields.operators,
+			}
+			if got := c.solve(tt.args.mathOperators); got != tt.want {
+				t.Errorf("solve() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCalibratorEquations_IsCheckValueReachable(t *testing.T) {
+	type fields struct {
+		checkValue int
+		operators  []int
+	}
+	tests := []struct {
+		name             string
+		fields           fields
+		mathOperatorList []string
+		want             bool
+	}{
+		{
+			name: "AOC task 01 Reachable Example",
+			fields: fields{
+				checkValue: 3267,
+				operators:  []int{81, 40, 27},
+			},
+			mathOperatorList: legalOperatorsTask01,
+			want:             true,
+		},
+		{
+			name: "AOC task 01 Not Reachable Example",
+			fields: fields{
+				checkValue: 7290,
+				operators:  []int{6, 8, 6, 15},
+			},
+			mathOperatorList: legalOperatorsTask01,
+			want:             false,
+		},
+		{
+			name: "AOC task 02 Reachable Example",
+			fields: fields{
+				checkValue: 7290,
+				operators:  []int{6, 8, 6, 15},
+			},
+			mathOperatorList: legalOperatorsTask02,
+			want:             true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &CalibratorEquations{
+				checkValue: tt.fields.checkValue,
+				operators:  tt.fields.operators,
+			}
+			if got := c.IsCheckValueReachable(tt.mathOperatorList); got != tt.want {
+				t.Errorf("IsCheckValueReachable() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_task01(t *testing.T) {
+	lines, err := util.ReadFilePerLine("../../input/day07_example.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ces := make([]*CalibratorEquations, 0)
+	for i := 0; i < len(lines); i++ {
+		ces = append(ces, newCalibratorEquations(lines[i]))
+	}
+
+	tests := []struct {
+		name string
+		want int
+	}{
+		{
+			name: "AOC Example",
+			want: 3749,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := task01(ces); got != tt.want {
+				t.Errorf("task01() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_task02(t *testing.T) {
+	lines, err := util.ReadFilePerLine("../../input/day07_example.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ces := make([]*CalibratorEquations, 0)
+	for i := 0; i < len(lines); i++ {
+		ces = append(ces, newCalibratorEquations(lines[i]))
+	}
+
+	tests := []struct {
+		name string
+		want int
+	}{
+		{
+			name: "AOC Example",
+			want: 11387,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := task02(ces); got != tt.want {
+				t.Errorf("task02() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
