007ceabc1971d0b3e6063a178757361326574668
stefan-ctrl <stefan-siber@t-online.de>
2025-02-14 13:13:51 +0100

    feat: day 09- task 2 in progress

--- days/09/day09.go
+++ days/09/day09.go
@@ -9,6 +9,9 @@ import (
 
 type FileId int
 
+var writtenBlocks = make(map[int]bool)
+var movedBlocks = make(map[int]bool)
+
 func main() {
 	lines, err := util.ReadFilePerLine("./input/day09.txt")
 	if err != nil {
@@ -23,7 +26,11 @@ func main() {
 
 func task01(line string) int {
 	fileBlocks := toFileBlocks(line)
+	return checksum(fileBlocks)
+}
 
+func task02(line string) int {
+	fileBlocks := toFileBlocksComplete(line)
 	return checksum(fileBlocks)
 }
 
@@ -62,10 +69,70 @@ func toFileBlocks(str string) []FileId {
 	return fileBlocks
 }
 
+func toFileBlocksComplete(str string) []FileId {
+	fileBlocks := make([]FileId, 0)
+	reverse_index := len(str) - 1
+	var blockSizeToWrite int
+	var appendGap bool
+	reverseBlockSizeToWrite, _ := strconv.Atoi(string(str[reverse_index]))
+	for i := range str {
+		blockSizeToWrite, _ = strconv.Atoi(string(str[i]))
+		var extendTo int
+		if i == reverse_index {
+			extendTo = len(fileBlocks) + reverseBlockSizeToWrite
+		} else {
+			extendTo = len(fileBlocks) + blockSizeToWrite
+		}
+		for len(fileBlocks) < extendTo && !appendGap {
+			if isFileAndNotMoved(i) {
+				fileBlocks = append(fileBlocks, FileId(i/2))
+				writtenBlocks[i] = true
+			} else if isFileMoved(i) {
+				fileBlocks = append(fileBlocks, -1)
+			} else {
+				for !isFileAndNotMoved(reverse_index) {
+					reverse_index--
+					if reverse_index >= 0 {
+						reverseBlockSizeToWrite, _ = strconv.Atoi(string(str[reverse_index]))
+					} else {
+						appendGap = true
+					}
+				}
+				if extendTo-len(fileBlocks) >= reverseBlockSizeToWrite {
+					fileBlocks = append(fileBlocks, FileId(reverse_index/2))
+					reverseBlockSizeToWrite--
+					if reverseBlockSizeToWrite == 0 {
+						movedBlocks[reverse_index] = true
+						writtenBlocks[reverse_index] = true
+						reverse_index--
+					}
+				} else {
+					reverse_index--
+				}
+				if appendGap {
+					fileBlocks = append(fileBlocks, -1)
+				}
+			}
+		}
+		if len(writtenBlocks)*2 >= len(str) {
+			break
+		} else {
+			reverse_index = len(str) - 1
+			reverseBlockSizeToWrite, _ = strconv.Atoi(string(str[reverse_index]))
+			appendGap = false
+		}
+	}
+	return fileBlocks
+}
+
 func checksum(blocks []FileId) int {
 	sum := 0
 	for i := range blocks {
-		sum += i * int(blocks[i])
+		v := blocks[i]
+		if blocks[i] == -1 {
+			v = 0
+		}
+		sum += i * int(v)
 	}
 	return sum
 }
@@ -74,6 +141,11 @@ func isFile(i int) bool {
 	return i%2 == 0
 }
 
-func task02(line string) int {
-	return len(line)
+func isFileAndNotMoved(i int) bool {
+	return i%2 == 0 && !isFileMoved(i)
+}
+
+func isFileMoved(i int) bool {
+	_, found := movedBlocks[i]
+	return found
 }
--- days/09/day09_test.go
+++ days/09/day09_test.go
@@ -72,3 +72,71 @@ func Test_task01(t *testing.T) {
 		})
 	}
 }
+
+func Test_task02(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "AoC Example",
+			args: args{
+				line: "2333133121414131402",
+			},
+			want: 2858,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := task02(tt.args.line); got != tt.want {
+				t.Errorf("task01() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_toFileBlocksComplete(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []FileId
+	}{
+		{
+			name: "AoC Example Task Two",
+			args: args{
+				line: "2333133121414131402",
+			},
+			want: []FileId{
+				0, 0,
+				9, 9,
+				2,
+				1, 1, 1,
+				7, 7, 7,
+				-1,
+				4, 4,
+				-1,
+				3, 3, 3,
+				-1, -1, -1, -1,
+				5, 5, 5, 5,
+				-1,
+				6, 6, 6, 6,
+				-1, -1, -1, -1, -1,
+				8, 8, 8, 8,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toFileBlocksComplete(tt.args.line); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("toFileBlocksComplete() = \n%v, want \n%v", got, tt.want)
+			}
+		})
+	}
+}
